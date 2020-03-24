20e4c181725605d3054a3873194083c20aa4748e
David <n.dave.hernandez@gmail.com>
2020-03-24 01:22:19 -0400

    Arrays/Creating Arrays

--- 3. Arrays /index.js
+++ 3. Arrays /index.js
@@ -1,1 +1,10 @@
-alert('Arrays');
+// 1. ARRAYS
+
+// A. CREATING ARRAYS
+
+let soccerTeams = []; // Empty Array
+let season = ['Spring', 'Summer', 'Autumn', 'Winter']; // Array of Strings
+let codeEmployees = [12, 23, 45, 56]; // Array of numbers
+let stuff = [true, 78, 'dog', null]; // Mixed array
+
+// new Array(1,2,3,4) - create new array in the console
